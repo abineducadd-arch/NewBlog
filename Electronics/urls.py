d62f07468246e15a688d20d6b5c1f55533ffbd0d
abineducadd-arch <abineducadd@gmail.com>
2026-03-15 22:40:20 +0530

    Update urls.py

--- Electronics/urls.py
+++ Electronics/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Django Blog API is Running 🚀")
