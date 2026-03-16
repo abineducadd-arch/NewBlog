fe3df1283b4e7121f87e80af490e2c1f8b1ad71a
abineducadd-arch <abineducadd@gmail.com>
2026-03-16 12:28:04 +0530

    all change

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --on-input
+python manage.py collectstatic --noinput
 
-python manage.py migrate 
+python manage.py migrate
+
+if [[ "$CREATE_SUPERUSER" == "True" ]]; then
+    python manage.py createsuperuser --noinput
+fi
